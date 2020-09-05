6e39cd09703291bd3aab3fe4de7b05e673262afb
JulienGuinot <45630763+Pliploop@users.noreply.github.com>
2020-09-05 23:19:38 +0200

    wtf is going on

--- Sudoku_Boardclass.py
+++ Sudoku_Boardclass.py
@@ -5,10 +5,22 @@ class board:
 
     def __init__(self,size):
         self.size=size
-        self.b= [[0 for k in range(size)] for k in range(size)]
+        self.b= [[0 for k in range(size)] for k in range(size)] 
 
-    def changeval(self,x,y,val):
-        self.b[x][y]=val
+    def get_size(self):
+        return self.size
+
+    # def find_empty(self):
+    #     n=self.size
+    #     for k in range(n) :
+    #         for i in range(n) :
+    #             temp=self.b[k][i]
+    #             print(temp)
+    #             if temp==0:
+    #                 return k,i
+    
+    def changeval(self,i,k,val):
+        print("ok")
 
     def printboard(self):
         stringboard = self.b
@@ -18,18 +30,15 @@ class board:
                 stringboard[k][i] = "{}".format(temp)
         for k in range(len(stringboard)):
             if k == 0:
-                print("------------------")
+                print("-------------------------")
             row = ''
             for i in range(len(stringboard)):
                 if i % 3 == 0:
                     row += '| '
                 row += stringboard[k][i]+' '
-                if i == len(stringboard):
-                    row += '| '
+            row+='|'    
             print(row)
             if ((k+1) % 3 == 0 and k != 0) or k == len(stringboard):
-                print("------------------")
+                print("-------------------------")
 
 
-gameboard=board(9)
-gameboard.printboard()
---
+++ Sudoku_game.py
@@ -0,0 +1,6 @@
+from Sudoku_Boardclass import board
+
+gameboard=board(9)
+gameboard.printboard()
+# gameboard.find_empty()
+gameboard.changeval(1,2,3)
