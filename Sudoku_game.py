f1e770d005ca40a65ec875d03ddac476ba21f814
JulienGuinot <45630763+Pliploop@users.noreply.github.com>
2020-09-06 12:02:45 +0200

    working GUI not shwing items yet

--- Sudoku_game.py
+++ Sudoku_game.py
@@ -1,7 +1,49 @@
 from Sudoku_Boardclass import board
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore
 
+
+#Setting up the board
 gameboard=board(9)
 gameboard.changeval(0,0,1)
-gameboard.printboard()
-gameboard.solve()
-gameboard.printboard()
+
+#Setting up the layout
+Game=QApplication([])
+window=QWidget()
+layout=QVBoxLayout()
+
+
+table=QTableWidget(9,9)
+for k in range(gameboard.size):
+    table.setRowHeight(k,30)
+    for i in range(gameboard.size):
+        table.setItem(k,i, QTableWidgetItem(gameboard.b[k][i])) 
+        table.setColumnWidth(i,30)
+table.horizontalHeader().hide()
+table.verticalHeader().hide()
+table.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+table.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+table.verticalScrollBar().setDisabled(True)
+table.horizontalScrollBar().setDisabled(True)
+# table.verticalScrollBar().hide()
+# table.horizontalScrollBar().hide()
+table.setFixedWidth(273)
+table.setFixedHeight(273)
+
+## Getting widgets and their connectionsA
+SolveButton=QPushButton('Solve')
+Showbutton=QPushButton('Show grid')
+
+##connections
+SolveButton.clicked.connect(gameboard.solve)
+Showbutton.clicked.connect(gameboard.printboard)
+
+layout.addWidget(table)
+layout.addWidget(SolveButton)
+layout.addWidget(Showbutton)
+
+window.setLayout(layout)
+window.show()
+Game.exec_()
+
+
